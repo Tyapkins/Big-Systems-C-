e02361e1650f2d38e6ff47b4bc0d51f3c9cbeb17
s02180538 <komrad.streln@yandex.ru>
2021-09-25 15:43:28 +0300

    Some fixes with task 2

--- Homework 2/task2.cpp
+++ Homework 2/task2.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
-#include <typeinfo>
 #include <tuple>
 #include <type_traits>
 #include <set>
@@ -30,22 +29,21 @@ struct Printer {
     template <typename... Args>
     Printer& format(std::tuple<Args...>);
 
-    //template <typename T1, typename T2>
-    //void iter_print(T1<T2>);
+    template<typename T>
+    void iteration(T beg, T end);
+
 };
 
-
-
-/*template <typename T1, typename T2>
-void Printer::iter_print(T1<T2> param)
+template<typename T>
+void Printer::iteration(T beg, T end)
 {
-    for (auto i = param.begin(); i<param.end(); i++)
+    for (auto i = beg; i != end; i++)
     {
-        this -> format(*i);
-        if (std::next(i) != param.end())
+        this->format(*i);
+        if (std::next(i) != end)
             final_line << ", ";
     }
-}*/
+}
 
 template <typename T1, typename T2>
 Printer& Printer::format(std::pair<T1, T2> param)
@@ -55,12 +53,13 @@ Printer& Printer::format(std::pair<T1, T2> param)
     final_line << ", ";
     this->format(param.second);
     final_line << ")";
+    return *this;
 }
 
 template <typename T>
-Printer& Printer::format(T&& pam)
-{
-    final_line << pam;
+Printer& Printer::format(T&& param)
+{//since nothing was said about special string handling, their output does not change
+    final_line << param;
     return *this;
 }
 
@@ -72,7 +71,12 @@ Printer& Printer::format(std::tuple<Args...> param)
     std::apply([this, &counter, max_size](auto&&... args)
     {
         (
-            (this->format(args), final_line << (++counter < max_size ? ", " : "")), ...);}, param);
+            (this->format(args),
+                    final_line << (++counter < max_size ? ", " : "")),
+            ...
+        );
+   },
+        param);
     final_line << " )";
     return *this;
 }
@@ -81,12 +85,7 @@ template <typename T>
 Printer& Printer::format(std::vector<T> param)
 {
     final_line << "[ ";
-    for (auto i = param.begin(); i<param.end(); i++)
-    {
-        this -> format(*i);
-        if (std::next(i) != param.end())
-            final_line << ", ";
-    }
+    iteration(param.begin(), param.end());
     final_line << " ]";
     return *this;
 }
@@ -95,12 +94,7 @@ template <typename T>
 Printer& Printer::format(std::set<T> param)
 {
     final_line << "{ ";
-    for (auto i = param.begin(); i<param.end(); i++)
-    {
-        this -> format(*i);
-        if (std::next(i) != param.end())
-            final_line << ", ";
-    }
+    iteration(param.begin(), param.end());
     final_line << " }";
     return *this;
 }
@@ -115,6 +109,15 @@ int main() {
     std::tuple<std::string, int, int> t = {"xyz", 1, 2};
     std::vector<std::pair<int, int> > v = {{1, 4}, {5, 6}};
 
+    std::vector<std::set<std::pair<int,int>>> vec = { {{4,5}, {2, 6}, {10, 7}}, {{123, 11},
+                                                                                 {14, 14}},
+                                                      {{123, 123}, {1, 1}, {2, 2}, {10,10}}};
+
+    std::tuple<decltype(vec), float, int, std::set<std::vector<std::string>>> super_vec = {vec, 0.5, 11,
+                                                                                           {{"pum", "pum", "pam"},
+                                                                                            {"check", "test", "oh no"},
+                                                                                            {"let's see..."}
+                                                                                           }};
 
     std::string s1 = Printer().format(" vector: ").format(v).str();
     std::cout << s1 << std::endl;
@@ -122,6 +125,12 @@ int main() {
     std::string s2 = Printer().format(t).format(" ! ").format(0).str();
 // "( xyz, 1, 2 ) ! 0"
     std::cout << s2 << std::endl;
+
+    std::string s3 = Printer().format(" Scary one: ").format(vec).str();
+    std::cout << s3 << std::endl;
+
+    std::string s4 = Printer().format("Behold! Something terrible: ").format(super_vec)
+            .format("\n And that's not all! ").format(14).format(" ").format(s1).format(" and... ").format(0.5).str();
+
+    std::cout << s4 << std::endl;
 }
-
-

9c3fd8812cc97f657b4f69c1965f38a505713661
s02180538 <komrad.streln@yandex.ru>
2021-12-02 06:14:33 +0300

    Print funciton distinguished

--- Homework 5/main.cpp
+++ Homework 5/main.cpp
@@ -1,51 +1,37 @@
 #include "factory.h"
 
+template<typename T>
+void print_distr(const std::string& name, T values)
+{
+    std::cout << "Checking " << name << " distribution" << std::endl;
+
+    for (auto val: values) {
+        auto pair_val = check_distrib(name, val);
+        std::cout << pair_val.first << " : " << pair_val.second << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+}
+
 int main()
 {
     std::vector<double> pois_vals = {0.1, 0.5, 1.0, 2.5, 5.0, 10.0, 17.1, 25.3, 93.546, 7};
-    std::cout << "Checking Poisson distribution:" << std::endl;
-
-    for (auto val: pois_vals) {
-        auto pam = check_distrib("poisson", val);
-        std::cout << pam.first << " : " << pam.second << std::endl;
-    }
-
-    std::cout << std::endl << std::endl;
+    print_distr("poisson", pois_vals);
 
     std::vector<double> bern_vals = {};
     int max_num = 10;
     for (int i = 0; i <= max_num; i++)
         bern_vals.push_back(double(i)/max_num);
 
-
-    std::cout << "Checking Bernoulli distribution:" << std::endl;
-    for (auto val: bern_vals) {
-        auto pam = check_distrib("bernoulli", val);
-        std::cout << pam.first << " : " << pam.second << std::endl;
-    }
-
-    std::cout << std::endl << std::endl;
-
-    std::cout << "Checking Geometric distribution:" << std::endl;
-    for (auto val: bern_vals) {
-        auto pam = check_distrib("geometric", val);
-        std::cout << pam.first << " : " << pam.second << std::endl;
-    }
-
-    std::cout << std::endl << std::endl;
-
+    print_distr("bernoulli", bern_vals);
+    print_distr("geometric", bern_vals);
 
     std::vector<fin_distr> fin_check =
             {{{1, 5, 10}, {0.1, 0.5, 0.4}}, {{0, 1}, {0.3, 0.7}}, {{100, 55, 32}, {0.3, 0.3, 0.4}},
              {{2, 5, 6, 10, 100}, {0.25, 0.4, 0.15, 0.15, 0.05}}, {{2, 5, 6, 10, 100}, {0.2, 0.2, 0.2, 0.2, 0.2}},
              {{10, 100, 1000}, {0.3, 0.3, 0.4}}, {{10, 100, 1000}, {0.6, 0.35, 0.05}}};
 
-    std::cout << "Checking Finite distribution:" << std::endl;
-    for (const auto& val : fin_check) {
-        auto pam = check_distrib("finite", val);
-        std::cout << pam.first << " : " << pam.second << std::endl;
-    }
-
+    print_distr("finite", fin_check);
+    
     return 0;
 }
 
